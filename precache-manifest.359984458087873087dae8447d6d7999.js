7105f120d7045c90e4ce02214c8d6c4caf632505
Albert Silva <albertsilva@Alberts-Air.hsd1.ut.comcast.net>
2019-08-27 21:45:31 -0600

    Updates

---
+++ precache-manifest.359984458087873087dae8447d6d7999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6623bf584c0a956b820bee1136527cef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ebe84f8783a2591cfd32",
+    "url": "/monsters-rolodex/static/css/main.43f2727e.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "ebe84f8783a2591cfd32",
+    "url": "/monsters-rolodex/static/js/main.68a25c09.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
